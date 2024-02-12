a65b57c4ffe66ac425a1a3dda98675f96025f77b
FazilAlauddeen <fazilala14@gmail.com>
2024-02-12 19:44:01 +0530

    first commit

---
+++ src/main/java/com/bookStore/entity/Book.java
@@ -0,0 +1,68 @@
+package com.bookStore.entity;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "BOOK")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "AUTHOR")
+	private String author;
+	@Column(name = "PRICE")
+	private String price;
+	public Book(int id, String name, String author, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+	public Book() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+	
+
+}
